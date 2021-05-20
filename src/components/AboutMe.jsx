4a059f57339688d578f9d70d447b8266edb424b1
Zachary Kramer <zckramer83@gmail.com>
2021-05-20 14:37:41 -0400

    breathing new life into react portfolio

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,6 +4,13 @@ export default function AboutMe (props) {
     return (
         <div className="About-Me">
             <h2>About me...</h2>
+            <p id='About-Me__Text'>Since 2014 I have been studying video game design, and looking for employment in an Agile, and 
+        TDD based environment. I began my journey by attending Full Sail University's online Video Game Design 
+        program where I learned C#, and practiced video game development in Unity. After graduating in 2017, 
+        earning a Bachelors degree of Science, I studied JavaScript, HTML and CSS in my spare time. When I was 
+        met with competition in my job search, I decided to update my skills set and get training in full stack 
+        development by attending We Can Code IT, in the fall of 2019. I graduated December 11th, 2019 with a
+        certification in Software Development</p>
         </div>
     )
 }
--- src/components/MyProjects.jsx
+++ src/components/MyProjects.jsx
@@ -4,6 +4,15 @@ export default function AboutMe (props) {
     return (
         <div className="My-Projects">
             <h2>My Projects</h2>
+            <div id="Projects-Bar">
+                <div className="Projects-Bar__Item">Tank's Ironclad Meats</div>
+                |
+                <div className="Projects-Bar__Item">Penguin Rush</div>
+                |
+                <div className="Projects-Bar__Item">The Nav-Bar</div>
+                |
+                <div className="Projects-Bar__Item">Your Website Here!</div>
+            </div>
         </div>
     )
 }
