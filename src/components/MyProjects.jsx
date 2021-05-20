ab2973ec1e34a7a7c17a16ee6f36c0e52979d455
Zachary Kramer <zckramer83@gmail.com>
2021-05-20 15:18:48 -0400

    basic project navigation done

--- src/components/MyProjects.jsx
+++ src/components/MyProjects.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Tanks from './ProjectTanks';
+import NavBar from './ProjectNavBar';
 
-export default function AboutMe (props) {
+const AboutMe = (props) => {
+    const [projectFocus, setProjectFocus] = useState(<div>initial focus</div>)
+    
+    function changeProjectFocus (project) {
+        setProjectFocus(project);
+    }
+
     return (
         <div className="My-Projects">
             <h2>My Projects</h2>
             <div id="Projects-Bar">
-                <div className="Projects-Bar__Item">Tank's Ironclad Meats</div>
+                <div className="Projects-Bar__Item" onClick={()=>changeProjectFocus(Tanks)}>Tank's Ironclad Meats</div>
                 |
                 <div className="Projects-Bar__Item">Penguin Rush</div>
                 |
-                <div className="Projects-Bar__Item">The Nav-Bar</div>
+                <div className="Projects-Bar__Item" onClick={()=>changeProjectFocus(NavBar)}>The Nav-Bar</div>
                 |
                 <div className="Projects-Bar__Item">Your Website Here!</div>
+            </div>
+            <div id="Project-Panel">
+                {projectFocus}
             </div>
         </div>
     )
 }
+export default AboutMe;
---
+++ src/components/ProjectTanks.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import kitty from '../images/kitten_500x300.jpg';
+
+const Tanks = (props) => {
+    return (
+        <div className='Project__Tanks'>
+            Tank's Ironclad meats project description
+            <img src={kitty} />
+        </div>
+    )
+}
+
+export default Tanks;
