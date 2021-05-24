fa01936a0d0851aac7d785eb2263376a824f5759
Zachary Kramer <zckramer83@gmail.com>
2021-05-24 14:47:35 -0400

    added a silly little image wheel thingy to Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import headshot from '../images/zack-headshot.jpg';
 import reactImage from '../images/react.webp';
 import mongoImage from '../images/mongoDB.png';
 import nodejsImage from '../images/nodeJS.png';
-
+import css3Image from '../images/css3.png';
 
 export default function Home (props) {
     return (
@@ -25,7 +25,7 @@ export default function Home (props) {
                     <img className='ImageWheel__Item' id='mongoImage' src={mongoImage} alt='Turning wheel representing technical skills' />
                     <img className='ImageWheel__Item' id='reactImage' src={reactImage} alt='Turning wheel representing technical skills' />
                     <img className='ImageWheel__Item' id='nodeImage' src={nodejsImage} alt='Turning wheel representing technical skills' />
-                    {/* <img className='ImageWheel__Item' id='image__headshot' src={headshot} alt='Turning wheel representing technical skills' /> */}
+                    <img className='ImageWheel__Item' id='cssImage' src={css3Image} alt='Turning wheel representing technical skills' />
                 </div>
             </div>
         </div>
