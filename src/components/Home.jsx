742bd4b0c6a6fc63f6c9f0cc655f89a9f2f81451
Zachary Kramer <zckramer83@gmail.com>
2021-05-24 17:50:41 -0400

    fixed home image wheel

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,23 +9,25 @@ export default function Home (props) {
     return (
         <div className="Home" >
             <div className="Home__Header">
+                
                 <h2>Zack Kramer</h2>
                 <img 
                     src={headshot} 
                     alt="a headshot of the developer being showcased"
                     id="image__headshot"
                     onClick={props.about}
+                    style={{zIndex:2}}
                     />
-                <div className="Home__Intro">
-                    <p>dev. gamer. bon vivant.</p>
-                </div>
-            </div>
             <div className='Home__ImageWheel'>
                 <div className='ImageWheel'>
                     <img className='ImageWheel__Item' id='mongoImage' src={mongoImage} alt='Turning wheel representing technical skills' />
                     <img className='ImageWheel__Item' id='reactImage' src={reactImage} alt='Turning wheel representing technical skills' />
                     <img className='ImageWheel__Item' id='nodeImage' src={nodejsImage} alt='Turning wheel representing technical skills' />
                     <img className='ImageWheel__Item' id='cssImage' src={css3Image} alt='Turning wheel representing technical skills' />
+                </div>
+            </div>
+                <div className="Home__Intro">
+                    <p style={{zIndex:0}}>dev. gamer. bon vivant.</p>
                 </div>
             </div>
         </div>
