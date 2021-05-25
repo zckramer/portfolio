0cc171b1f12f0b528f2d7b09a206e5c11eeddcb7
Zachary Kramer <zckramer83@gmail.com>
2021-05-25 13:59:11 -0400

    added tanks section

---
+++ src/components/ImageCarousel/ImageCarousel.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+const ImageCarousel = (props) => {
+    const [imageFromCarousel, setImageFromCarousel] = useState(props.images[0])   
+    
+    let carouselIndex = 0;
+    
+    function carouselIncrement () {
+        carouselIndex >= props.images.length - 1 ? carouselIndex = 0 : carouselIndex = carouselIndex + 1;
+    }
+
+    useEffect(() => {
+        setInterval(()=> {
+            carouselIncrement();
+            setImageFromCarousel(props.images[carouselIndex])
+        }, 4000);
+        return clearInterval();
+    }, []);
+
+    return (
+        <div 
+            className='Image-Carousel'
+            style={{
+                display:'flex',
+                flexDirection:'row',
+                alignItems:'center',
+                justifyContent:'space-evenly',
+                width:'70%',
+                height: '70%'
+            }}
+        >
+        <img 
+            className='Image-Carousel__Image' 
+            src={imageFromCarousel} 
+            alt='product cooking and the purchased product' 
+            style={{
+                position:'relative',
+                width:'60%'
+            }}
+        />
+        <div 
+            className='Image-Carousel__Caption'
+            style={{
+                position:'absolute',
+                bottom:'-10%',
+                left: '20%',
+                marginLeft:'auto',
+                marginRight:'auto'
+            }}
+        >
+                <p>{props.caption}</p>
+        </div>
+        <div 
+            className='Image-Carousel__Image-Description'
+            style={{
+                position:'block',
+                right:'0%',
+                maxWidth:'40%'
+
+            }}
+        >
+            {props.children}
+        </div>
+    </div>
+    )
+}
+
+export default ImageCarousel;
--- src/components/ProjectTanks.jsx
+++ src/components/ProjectTanks.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import ImageCarousel from './ImageCarousel/ImageCarousel';
+import image01 from '../images/tanks/FireAndSmoke01.JPG';
+import image02 from '../images/tanks/Rotisserie01.JPG';
+import image03 from '../images/tanks/Ribs01.JPG';
+import image04 from '../images/tanks/Chicken_Plate01.JPG';
+import image05 from '../images/tanks/PulledPork_Plate01.JPG';
+import image06 from '../images/tanks/RibsAndChicken_Plate01.JPG';
 
 const Tanks = () => {
+ 
     return (
         <div className='Project-Panel'>
-            Tank's Ironclad meats project description
+            <ImageCarousel 
+                images={[image01, image02, image03, image04, image05, image06]}
+                caption={<a href='https://www.tanksironcladmeats.com'>Tank's Ironclad Meats</a>}
+            >
+                <p>
+                    Design, Production, and Development of Tank's Ironclad
+                    Meats by Zack Kramer, Photography by Nadia Swenson
+                </p>
+                <hr/>
+                <p>
+                    Tank's site was built with React, has responsive styling, and will continue to improve and grow
+                    alongside the company. It is an ongoing project for a company in my hometown of Columbus, Ohio. Since the company was started
+                    by my two friends, neither of which are artists, photographers, or graphic
+                    designers, I had to create all photos and videos myself. Additionally, I
+                    designed the website without any creative guidance.
+                </p>
+            </ImageCarousel>
         </div>
     )
 }
